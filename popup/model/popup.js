9b05408c3c9dd94d2c611b50decd0906212289a0
tommyrharper <thomasrobertharper@gmail.com>
2021-02-09 17:10:39 +0000

    Remove logs from popup.js

--- popup/model/popup.js
+++ popup/model/popup.js
@@ -4,11 +4,9 @@ const slider = document.getElementById('slider');
 let port = chrome.extension.connect({
   name: "popup-channel",
 });
+
 port.postMessage("Hi BackGround from popup.js");
-port.onMessage.addListener(function (msg) {
-  console.log("Received from background.js: ", msg);
-  toggle.checked = msg;
-});
+port.onMessage.addListener(function (msg) { toggle.checked = msg; });
 
 toggle.addEventListener('click', () => {
   port.postMessage(toggle.checked);

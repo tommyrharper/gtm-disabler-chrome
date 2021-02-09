11a799ea80a07dfd3c9941ceb5fa18903da2b848
tommyrharper <thomasrobertharper@gmail.com>
2021-02-09 15:09:24 +0000

    Update popup.js

--- popup/model/popup.js
+++ popup/model/popup.js
@@ -1,8 +1,46 @@
+// const toggle = document.getElementById('toggle');
+// let enabled = true
+
+// const body = document.body;
+
+// toggle.checked = enabled;
+
+// toggle.addEventListener('click', () => {
+//   console.log('clicked');
+//   enabled = !enabled
+//   console.log('enabled', enabled);
+//   let newDiv = document.createElement("div");
+//   newDiv.innerHTML = 'asdfsda';
+//   body.appendChild(newDiv);
+// })
+
+
+// let port = chrome.extension.connect({
+//   name: "Sample Communication",
+// });
+// port.postMessage("Hi BackGround from popup.js");
+// port.onMessage.addListener(function (msg) {
+//   console.log("Received from background.js: ", msg);
+// });
+
 const toggle = document.getElementById('toggle');
-let enabled = true
+
+let port = chrome.extension.connect({
+  name: "Sample Communication",
+});
+port.postMessage("Hi BackGround from popup.js");
+port.onMessage.addListener(function (msg) {
+  console.log("Received from background.js: ", msg);
+  toggle.checked = msg;
+});
 
 toggle.addEventListener('click', () => {
+  port.postMessage(toggle.checked);
   console.log('clicked');
-  enabled = !enabled
-  console.log('enabled', enabled);
+  console.log('toggle.checked', toggle.checked)
+  // enabled = !enabled
+  // console.log('enabled', enabled);
+  // let newDiv = document.createElement("div");
+  // newDiv.innerHTML = 'asdfsda';
+  // body.appendChild(newDiv);
 })
